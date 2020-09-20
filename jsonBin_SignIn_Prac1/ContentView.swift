dcf6a8072894e4b629216307c9cd4504c0efbf5f
tpubz <tpubz@icloud.com>
2020-09-20 13:52:59 -0600

    Initial Commit

---
+++ jsonBin_SignIn_Prac1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  jsonBin_SignIn_Prac1
+//
+//  Created by Taylor Pubins on 9/20/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ jsonBin_SignIn_Prac1/jsonBin_SignIn_Prac1App.swift
@@ -0,0 +1,17 @@
+//
+//  jsonBin_SignIn_Prac1App.swift
+//  jsonBin_SignIn_Prac1
+//
+//  Created by Taylor Pubins on 9/20/20.
+//
+
+import SwiftUI
+
+@main
+struct jsonBin_SignIn_Prac1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
